0279fbf7c931e2e94ad7e7ab16ab612d414ab91a
hanax <alfalfa.xue@gmail.com>
2015-09-21 15:49:27 -0400

    several project description changes

--- src/views/data.js
+++ src/views/data.js
@@ -54,28 +54,42 @@ var arts = [
 
 var techs = [
   {
-    name: 'FYI: Find Your Instructor',
-    image: '/assets/techs/00.jpg',
-    desc: 'At the beginning of 2014, I started to work on FYI (Find Your Instructor) with my friends. FYI is a website providing information of professors and schools for CS undergrads who want to apply for graduate schools in the US.'
-          + '\n\nThe website is powered by Node.js + MongoDB. Professor data are crawled from sources including university websites, wikipedia and their personal webistes.',
-    color: '#22cde7'
-  },
-  {
     name: 'Shake It',
     image: '/assets/techs/01.jpg',
-    desc: 'Shake It is a gesture based tinder style coupon recommendation web app.'
+    desc: 'Shake It is a gesture based tinder style coupon recommendation web app. I designed and developed the entire application.'
           + '\n\nThe app has a collaborative filtering recommendation system based on user actions including starring, sharing or ignoring the coupons.'
           + '![Screenshot1](/assets/techs/shakeit_0.png)'
           + '![Screenshot1](/assets/techs/shakeit_1.png)',
     color: '#22b2e7'
   },
   {
+    name: 'Strolp',
+    image: '/assets/techs/strolp.jpg',
+    desc: 'Strolp tries to combine the best of both e-shopping and {physical shopping} and tries to strike a sweet balance which could be win-win for all the people involved. Selling in bulk has always been beneficial to the retailers. We often see a lot of deals on purchase of bulk quantity of goods rather than individual pieces. Tapping on this concept, with Strolp, retailers offer the best possible discounts when people visit them in numbers. Say brand X which generally sells its product for 100$ would sell it for 80$ if 10 people buy it in a group. This listing is made on Strolp. The shopper discovers such deals on the mobile application and indicates interest. Strolp takes the pain of finding other 9 people to avail the discount offered by brand X. As soon as there are enough number of people to avail the deal, a notification is sent to the shopper to visit the shop and make the purchase. This can be done in a group or individually. Say, you are new to the city or want to go shopping in a group to get some feedback, strolp has already found your friends!'
+          + '\n\nStrolp consists of a web interface for stores and an iOS app for users. The entire application with fully functional front-end and back-end is developed within 36 hours for Big Red Hackathon. I was responsible for all the design and front-end development work. '
+          + '\n\nPhoto of me from Cornell news presenting the demo of Strolp!'
+          + '![Screenshot1](/assets/techs/strolp_news.jpg)'
+          + '![Screenshot1](/assets/techs/strolp_m.png)'
+          + '\n\n[Know more about the App](http://devpost.com/software/strolp-bnco6t/)',
+    color: '#e89a1e'
+  },
+  {
     name: 'Strata: Scalable 2D Barcode',
     image: '/assets/techs/02.jpg',
-    desc: 'Strata is a novel scalable 2D barcode design. When scanning the barcode, user could get different layers of information based on their distance between the scanning device and the barcode.'
+    desc: 'Strata is a novel scalable 2D barcode design. In this project we try to explore the way people interact with barcods.'
+          + 'When scanning Strata barcode, instead of the all-or-nothing behavior of traditional QR Code, user could now get different layers of information based on their distance between the scanning device and the barcode.'
           + '\n\nThis project is a collaborative work with [MSRA](http://research.microsoft.com/en-us/labs/asia/) and [Yale University](http://www.yale.edu) and is accepted to [ACM MobiCom 2014](http://www.sigmobile.org/mobicom/2014/).'
           + '\n\n[Read Paper](/assets/strata.pdf)',
     color: '#e88822'
+  },
+  {
+    name: 'FYI: Find Your Instructor',
+    image: '/assets/techs/00.jpg',
+    desc: 'FYI is a website providing information of professors and schools for CS undergrads who want to apply for graduate schools in the US. This is a side project I made with classmates when I was at Peking University.'
+          + '\n\nThe website is powered by Node.js + MongoDB. Professor data are crawled from sources including university websites, wikipedia and their personal webistes.'
+          + '\n\nPoster for the project demo day:'
+          + '![Poster](/assets/techs/fyi_poster.png)',
+    color: '#22cde7'
   },
   {
     name: 'TeamGO',
@@ -85,7 +99,21 @@ var techs = [
           + '\n\n[Download the App](http://theteamgo.com/)'
           + '![Screenshot1](/assets/techs/teamgo_0.png)'
           + '![Screenshot1](/assets/techs/teamgo_1.png)',
-    color: '#22e7ba'
+    color: '#31b48e'
+  },
+  {
+    name: 'T-shirt Design',
+    image: '/assets/techs/05.jpg',
+    desc: 'During my undergrad years at Peking University, I have designed a couple T-shirts for thousands of students, and was lucky enough to see them being worn everywhere in the campus.'
+          + '\n\n2014 Graduation T-shirt for the school of EECS: If you try to compile the code in the drawing, it actually turns out like [THIS](/assets/techs/grad_14.html). This little "Bye PKU and Hello World" surprise I made has moved thousands of graduates and the T-shirt once went really popular on social networks.'
+          + '![Screenshot1](/assets/techs/grad_14.jpg)'
+          + '\n\n2013 Graduation T-shirt for the school of EECS: Over 300 students wore this for their graduation photo.'
+          + '![Screenshot1](/assets/techs/grad_13.jpg)'
+          + '\n\nA colored version of the front pattern:'
+          + '![Screenshot1](/assets/techs/grad_13_colored.jpg)'
+          + '\n\nT-shirt for Shanghainese at Peking University: As a proud Shanghai local, I tried to embed Huangpu River and the gorgeous skyline of Shanghai into the letters.'
+          + '![Screenshot1](/assets/techs/shanghai.jpg)',
+    color: '#3bc3cf'
   },
   {
     name: 'CS Classics',
